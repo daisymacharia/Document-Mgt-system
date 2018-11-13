96b99d64d3146106bfe2a8576a8174ca62ddb19a
daisymacharia <daisy.macharia@andela.com>
2018-11-13 16:55:20 +0300

    trial

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import router from "./src";
 import webpack from "webpack";
 import config from "./webpack.config";
-import webpackDevServer from "webpack-dev-server";
 
 const compiler = webpack(config);
 const app = express();
@@ -13,7 +12,11 @@ const options = {
 	host: "localhost"
 };
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 app.use(bodyParser.json());
 
 // Register the routes
